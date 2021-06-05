7eb1ca07023186b5f76335c66bd1c3f16b9b6b44
keksv <80920621+keksv@users.noreply.github.com>
2021-06-06 00:22:35 +0400

    lesson 71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
-import Preloader from "../../common/Preloader/Preloader";
+import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -10,12 +11,12 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div>
-                <img
-                    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
+                <span></span>
             </div>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                ava + description
+                <ProfileStatus status={"Hello!!!"}/>
+
             </div>
         </div>
     )
